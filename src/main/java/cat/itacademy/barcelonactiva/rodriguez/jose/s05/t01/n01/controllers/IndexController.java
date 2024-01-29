be841ca05231f2bcd9a501b052e3ab3f96afd9ff
Jose Antonio <josanrocu@gmail.com>
2024-01-29 12:17:17 +0100

    first commit

---
+++ src/main/java/cat/itacademy/barcelonactiva/rodriguez/jose/s05/t01/n01/controllers/IndexController.java
@@ -0,0 +1,2 @@
+package cat.itacademy.barcelonactiva.rodriguez.jose.s05.t01.n01.controllers;public class IndexController {
+}
---
+++ src/main/java/cat/itacademy/barcelonactiva/rodriguez/jose/s05/t01/n01/model/dto/SucursalDto.java
@@ -0,0 +1,2 @@
+package cat.itacademy.barcelonactiva.rodriguez.jose.s05.t01.n01.model.dto;public class SucursalDto {
+}
